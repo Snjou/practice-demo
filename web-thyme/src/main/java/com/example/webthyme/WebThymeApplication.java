0f8615f73f2fa758a8f5b10a2273ff4e6efec19b
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-06-28 20:47:01 +0800

    added springboot

---
+++ web-thyme/src/main/java/com/example/webthyme/WebThymeApplication.java
@@ -0,0 +1,13 @@
+package com.example.webthyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebThymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebThymeApplication.class, args);
+	}
+
+}
---
+++ web-thyme/src/test/java/com/example/webthyme/WebThymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webthyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebThymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
